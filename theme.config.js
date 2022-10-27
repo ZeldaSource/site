83b5f88321030c1e5ee259bc202faf4a4750496c
ZeldaSource <zeldasourceproject@gmail.com>
2022-10-27 18:25:44 -0400

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
   github: 'https://github.com/shuding/nextra',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' ',
+  titleSuffix: '-ZeldaSource',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">ZeldaSource</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        archiving every zelda release, ever.
       </span>
     </>
   ),
