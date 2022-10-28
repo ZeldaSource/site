7a16070be7d249bfbfd3c4aaa4e0e30cad1d5d5d
ZeldaSource <zeldasourceproject@gmail.com>
2022-10-28 09:26:57 -0400

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ export default {
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
   titleSuffix: ' - ZeldaSource',
   logo: (
-      <img src="https://user-images.githubusercontent.com/116841381/198591245-0c150079-577a-4608-82e3-1aee3258b5d8.png" width="100"/>
+      <img src="https://user-images.githubusercontent.com/116841381/198609219-a078cc84-abc2-42ff-bd94-f860e06067e1.png" width="100"/>
 ,
   ),
   head: (
