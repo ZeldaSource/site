5581d6d4bc07e210c0cd1f94fae5bf42fbeb02a6
ZeldaSource <zeldasourceproject@gmail.com>
2022-10-28 09:43:11 -0400

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -57,9 +57,7 @@ export default {
   prevLinks: true,
   nextLinks: true,
   footer: true,
-  footerText: <>ZeldaSource - 
-  let text = "A Project by Gabe Fletcher";
-  let result = text.link("https://bit.ly/gabefletch");
+  footerText: <img src="https://user-images.githubusercontent.com/116841381/198591245-0c150079-577a-4608-82e3-1aee3258b5d8.png" width="100"/>
 </>,
   unstable_faviconGlyph: '⚔️',
 }
