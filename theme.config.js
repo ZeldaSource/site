0a27a90cb68a3f7ea9322e8ed322b1c92c8c7e47
ZeldaSource <zeldasourceproject@gmail.com>
2022-10-28 09:44:23 -0400

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -58,6 +58,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerText: <img src="https://user-images.githubusercontent.com/116841381/198591245-0c150079-577a-4608-82e3-1aee3258b5d8.png" width="100"/>
-</>,
+,
   unstable_faviconGlyph: '⚔️',
 }
